d9045ad4012ca7a91cefae57e0ace4dbd9df3361
LmOpe <lawalmuhammedope@gmail.com>
2024-02-15 22:57:29 +0100

    Set the opened movie title as the page title

--- src/App.jsx
+++ src/App.jsx
@@ -126,13 +126,18 @@ export default function App() {
 
   useEffect(
     function () {
+      const controller = new AbortController();
+
       async function fetchMovies() {
         try {
           setIsLoading(true);
           setError("");
 
-          const res = await fetch(`http://www.omdbapi.com/?apikey=${KEY}
-      &s=${query}`);
+          const res = await fetch(
+            `http://www.omdbapi.com/?apikey=${KEY}
+      &s=${query}`,
+            { signal: controller.signal }
+          );
 
           if (!res.ok)
             throw new Error("Something went wrong with fetching movies");
@@ -141,8 +146,9 @@ export default function App() {
           if (data.Response === "False") throw new Error("Movie not found");
 
           setMovies(data.Search);
+          setError("");
         } catch (err) {
-          setError(err.message);
+          if (err.name !== "AbortError") setError(err.message);
         } finally {
           setIsLoading(false);
         }
@@ -153,6 +159,10 @@ export default function App() {
         return;
       }
       fetchMovies();
+
+      return function () {
+        controller.abort();
+      };
     },
     [query]
   );
@@ -296,6 +306,35 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
       getMovieDetails();
     },
     [selectedId]
+  );
+
+  useEffect(
+    function () {
+      if (!title) return;
+      document.title = `Movie | ${title}`;
+
+      return function () {
+        document.title = "usePopcorn";
+      };
+    },
+    [title]
+  );
+
+  useEffect(
+    function () {
+      function callback(e) {
+        if (e.code === "Escape") {
+          onCloseMovie();
+        }
+      }
+
+      document.addEventListener("keydown", callback);
+
+      return function () {
+        document.removeEventListener("keydown", callback);
+      };
+    },
+    [onCloseMovie]
   );
 
   return (
